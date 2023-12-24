e18e0b4b8d2b2d8b23335f19140f78f8b5855558
Eko Kurniadi <ekokurniadi.02@gmail.com>
2023-12-24 10:51:15 +0700

    finish get and create new post

---
+++ application/src/post/create.rs
@@ -0,0 +1,23 @@
+use diesel::RunQueryDsl;
+use domain::models::{NewPost, Post};
+use infrastructure::establish_connection;
+use rocket::{serde::json::Json, response::status::Created};
+use shared::response_models::{Response, ResponseBody};
+
+pub fn create_post(post:Json<NewPost>)->Created<String>{
+    use domain::schema::posts;
+
+    let post = post.into_inner();
+
+    match diesel::insert_into(posts::table).values(&post).get_result::<Post>(&mut establish_connection()){
+        Ok(post)=> {
+            let response = Response{body:ResponseBody::Post(post)};
+            Created::new("").tagged_body(serde_json::to_string(&response).unwrap())
+        },
+        Err(err)=> match err{
+            _=>{
+                panic!("Database error - {}",err);
+            }
+        }
+    }
+}
